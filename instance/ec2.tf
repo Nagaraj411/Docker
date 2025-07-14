0df0fd4e378a394ba3595f5e7f4902919805e99b
Nagaraj <nagarajuande255@gmail.com>
2025-07-14 13:02:50 +0530

    docker

---
+++ instance/ec2.tf
@@ -0,0 +1,33 @@
+# This script will run instance & security Group inbound rules & ourbound rules 
+resource "aws_instance" "roboshop" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [ aws_security_group.allow_all.id ] # Security Group ID
+  
+  tags = { # Tags for the instance
+    Name = "Docker" # Name of the instance
+  }
+}
+resource "aws_security_group" "allow_all" {
+    name        = "allow_all (New)"
+    description = "allow all traffic"
+
+    ingress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = {
+        Name = "allow-all (New)"
+    }
+}
