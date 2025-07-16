04d3aae96d472c6255235429b0e233ef511a9ec5
Nagaraj <nagarajuande255@gmail.com>
2025-07-16 11:30:10 +0530

    docker

--- instance/docker.sh
+++ instance/docker.sh
@@ -1,10 +1,11 @@
-sudo dnf -y install dnf-plugins-core
-sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+#!/bin/bash
 
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo usermod -aG docker ec2-user
+dnf -y install dnf-plugins-core
+dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+systemctl start docker
+systemctl enable docker
+usermod -aG docker ec2-user
 
 growpart /dev/nvme0n1 4
 lvextend -L +20G /dev/RootVG/rootVol
--- instance/provider.tf
+++ instance/provider.tf
@@ -1,22 +1,13 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
       version = "5.98.0"
     }
-  }
-
-  backend "s3" {
-    bucket       = "docker411" # This should match the bucket in your S3 bucket
-    key          = "docker"
-    region       = "us-east-1"
-    encrypt      = true
-    use_lockfile = true
-
   }
 }
 
 provider "aws" {
   # Configuration options
-  region = "us-east-1" # Specify the AWS region
+  region = "us-east-1"
 }
