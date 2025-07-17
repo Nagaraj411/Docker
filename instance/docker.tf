e075fec9a7fb353a2f43f2e46f5dbbf012cd4c17
Nagaraj <nagarajuande255@gmail.com>
2025-07-17 12:45:39 +0530

    docker

--- instance/docker.tf
+++ instance/docker.tf
@@ -1,7 +1,7 @@
 # This script will run instance & security Group inbound rules & ourbound rules 
 resource "aws_instance" "docker" {
   ami           = "ami-09c813fb71547fc4f"
-  instance_type = "t3.micro"
+  instance_type = "t3.medium" # Instance type, can be changed as needed
   vpc_security_group_ids = [aws_security_group.allow_all_docker.id] # Security Group ID
 
   root_block_device {
@@ -9,7 +9,7 @@ resource "aws_instance" "docker" {
     volume_type = "gp3" # or "gp2", depending on your preference
   }
 
-  user_data = file("docker.sh") # User data script to install Docker
+    user_data = file("docker.sh") # User data script to install Docker
   tags = { # Tags for the instance
     Name = "Docker" # Name of the instance
   }
